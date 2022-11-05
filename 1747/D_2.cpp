b4551ca120c5287cd5786fff4691cc9d6087aece
asubramanian08 <arjun.subramanian.08@gmail.com>
2022-11-04 21:01:29 -0400

    1747D edits

--- 1747/D_2.cpp
+++ 1747/D_2.cpp
@@ -9,19 +9,18 @@ int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    long long n, arr[200010], ps[200010], q, zeros[200010];
+    long long n, arr[200010], ps[200010], q, zeros[200010], prev[200010];
     cin >> n >> q;
+    ps[0] = zeros[0] = 0;
+    map<pair<int, int>, int> m;
     for (int i = 1; i <= n; i++)
+    {
         cin >> arr[i];
-    ps[0] = 0;
-    for (int i = 1; i <= n; i++)
         ps[i] = ps[i - 1] ^ arr[i];
-    map<pair<int, int>, vector<int>> m;
-    for (int i = 0; i <= n; i++)
-        m[{i % 2, ps[i]}].push_back(i);
-    zeros[0] = 0;
-    for (int i = 1; i <= n; i++)
         zeros[i] = zeros[i - 1] + (arr[i] == 0);
+        prev[i] = m.count({(i + 1) % 2, ps[i]}) ? m[{(i + 1) % 2, ps[i]}] : -1;
+        m[{i % 2, ps[i]}] = i;
+    }
     while (q--)
     {
         int l, r;
@@ -32,15 +31,10 @@ int main(void)
             cout << 0 << endl;
         else if ((r - l + 1) % 2 == 1 || arr[l] == 0 || arr[r] == 0)
             cout << 1 << endl;
+        else if (prev[r] != -1 && prev[r] > l)
+            cout << 2 << endl;
         else
-        {
-            vector<int> &v = m[{(r + 1) % 2, ps[r]}];
-            auto it = upper_bound(v.begin(), v.end(), r);
-            if (it != v.begin() && l < *prev(it))
-                cout << 2 << endl;
-            else
-                cout << -1 << endl;
-        }
+            cout << -1 << endl;
     }
     return 0;
 }
